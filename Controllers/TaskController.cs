c93e8e35d115c43ccd9d00ff7ebc505c9a4d9046
Direwolf <rahulsood707@gmail.com>
2025-10-24 19:15:17 +0530

    added and configured controllers

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Concurrent;
+using Basic_Task_Manager.DTOs;
+using Basic_Task_Manager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basic_Task_Manager.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TaskController : ControllerBase
+    {
+        private static readonly ConcurrentDictionary<Guid, TaskItem> tasks = new();
+
+        [HttpGet]
+        public ActionResult<ICollection<TaskItem>> GetAllTask()
+        {
+            return Ok(tasks.Values);
+        }
+
+        [HttpPost]
+        public ActionResult<TaskItem> CreateTask(TaskCreateDTO taskDTO)
+        {
+            if (string.IsNullOrWhiteSpace(taskDTO.Description))
+                return BadRequest("Description is required!");
+
+            var newTask = new TaskItem
+            {
+                Id = Guid.NewGuid(),
+                Description = taskDTO.Description,
+                IsCompleted = false
+            };
+
+            tasks[newTask.Id] = newTask;
+
+            return Ok(newTask);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<TaskItem> UpdateTask(Guid id, TaskUpdateDTO taskDTO)
+        {
+            if (string.IsNullOrWhiteSpace(taskDTO.Description))
+                return BadRequest("Description is required!");
+
+            if (!tasks.ContainsKey(id))
+                return NotFound();
+
+            tasks[id].Description = taskDTO.Description;
+            tasks[id].IsCompleted = taskDTO.IsCompleted;
+
+            return Ok(tasks[id]);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteTask(Guid id)
+        {
+            if (!tasks.TryRemove(id, out _))
+                return NoContent();
+
+            return NotFound();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -10,6 +11,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.Run();
 
